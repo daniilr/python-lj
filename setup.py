05729b69ae195a80849237ca0abde97b433b643f
daniilr <i@daniil-r.ru>
2014-08-21 17:53:48 +1100

    Few minor fixes

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 setup(
     name='lj',
     packages=['lj'],  # this must be the same as the name above
-    version='0.1',
-    description='Pythonb',
+    version='0.1.1',
+    description='A python realization of LiveJournal (LJ) API. Checkout https://github.com/daniilr/python-lj for more info',
     author='Original developer: David Lynch; Curent maintainer: Daniil Ryzhkov ',
     author_email='i@daniil-r.ru',
     url='https://github.com/daniilr/python-lj',
